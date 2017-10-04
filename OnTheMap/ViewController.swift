40c2f4194ea876857a5c19fe7c2f0dd8f11dad45
Jae-Seung Lee <jaeseung@gmail.com>
2017-10-04 19:00:30 -0400

    Set the secure text entry for passwordTextField

--- OnTheMap/ViewController.swift
+++ OnTheMap/ViewController.swift
@@ -16,6 +16,8 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // passwordTextField.isSecureTextEntry = true
     }
 
     override func didReceiveMemoryWarning() {
