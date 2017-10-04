8d0195b4c485e6e75fcd791a2c07019a58a20225
Jae-Seung Lee <jaeseung@gmail.com>
2017-10-04 18:10:24 -0400

    Initial commit Put textfields and a button into LoginViewController

--- OnTheMap/ViewController.swift
+++ OnTheMap/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
